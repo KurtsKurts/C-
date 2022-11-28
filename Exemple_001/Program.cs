86389d2302e0cfe30b5ebe7d6be01bf859af6cec
“KurtsKurts” <Alex23.89@mail.ru>
2022-11-28 14:55:14 +0700

    проба

---
+++ Exemple_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2Hello, World!");
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("2Hello, World!");
+Console.WriteLine("2Hello, World!");
+
+Console.WriteLine(username);
+Console.WriteLine("2Hello, World!");
